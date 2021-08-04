7358b153f165dfb47b2823860f90e25cb6c1220f
Tucker Gillbrand <tucker@gillbrand.com>
2021-08-04 08:40:10 -0700

    Initial Commit

---
+++ TruthOrDare/TruthOrDareApp.swift
@@ -0,0 +1,17 @@
+//
+//  TruthOrDareApp.swift
+//  TruthOrDare
+//
+//  Created by Tucker(School) on 8/4/21.
+//
+
+import SwiftUI
+
+@main
+struct TruthOrDareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
